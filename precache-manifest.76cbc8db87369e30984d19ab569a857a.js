b2e69ccf35bd09e719d18a63fea242319b024a55
Rustam Guliev <glvrst@gmail.com>
2019-12-14 16:16:10 +0300

    Dist subtree commit

---
+++ precache-manifest.76cbc8db87369e30984d19ab569a857a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e09aa19bd3614d80e3a0",
+    "url": "/familial-hyperlipidemia-calculator/css/app.55fee358.css"
+  },
+  {
+    "revision": "c31d6e07e59c981ace11",
+    "url": "/familial-hyperlipidemia-calculator/css/chunk-vendors.dde6007b.css"
+  },
+  {
+    "revision": "1618c77b6c5c11926819a8868a083031",
+    "url": "/familial-hyperlipidemia-calculator/fonts/materialdesignicons-webfont.1618c77b.ttf"
+  },
+  {
+    "revision": "927457ed7478ec7e1580a2b88116f9c3",
+    "url": "/familial-hyperlipidemia-calculator/fonts/materialdesignicons-webfont.927457ed.woff2"
+  },
+  {
+    "revision": "d6e3eba9b16453f51d4ef6e216bf89ff",
+    "url": "/familial-hyperlipidemia-calculator/fonts/materialdesignicons-webfont.d6e3eba9.woff"
+  },
+  {
+    "revision": "f81583fc4d8aa5aa2820976561ea8aec",
+    "url": "/familial-hyperlipidemia-calculator/fonts/materialdesignicons-webfont.f81583fc.eot"
+  },
+  {
+    "revision": "8543e4478f69fd4193d8252ad12c3102",
+    "url": "/familial-hyperlipidemia-calculator/img/bg_heart.8543e447.png"
+  },
+  {
+    "revision": "10c333806dc283a8d327106012377d72",
+    "url": "/familial-hyperlipidemia-calculator/img/logo.10c33380.png"
+  },
+  {
+    "revision": "90bac522bef17d97f06fbf35183cacf4",
+    "url": "/familial-hyperlipidemia-calculator/index.html"
+  },
+  {
+    "revision": "e09aa19bd3614d80e3a0",
+    "url": "/familial-hyperlipidemia-calculator/js/app.485d3aea.js"
+  },
+  {
+    "revision": "c31d6e07e59c981ace11",
+    "url": "/familial-hyperlipidemia-calculator/js/chunk-vendors.ba04f0fe.js"
+  },
+  {
+    "revision": "8fde990e6987f663a125803809c9ea79",
+    "url": "/familial-hyperlipidemia-calculator/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/familial-hyperlipidemia-calculator/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/familial-hyperlipidemia-calculator/precache-manifest.76cbc8db87369e30984d19ab569a857a.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "familial-hyperlipidemia-calculator"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
