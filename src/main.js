a497b2a6a840b1a7cab03f11cd92071bb690f273
Rustam Guliev <glvrst@gmail.com>
2019-12-14 16:11:52 +0300

    Add PWA

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 import '@mdi/font/css/materialdesignicons.min.css'
 import App from './App.vue'
+import './registerServiceWorker'
 
 Vue.use(Buefy)
 Vue.config.productionTip = false
